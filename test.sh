92c336c68ef247de9ce322529b772859576844b5
Joshua McKenty <jmckenty@gmail.com>
2019-08-11 17:12:57 -0700

    Build a basic docker image w/ CLI tools for concourse test runs.

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# create a cluster (via service broker)
+# create a DB against the cluster (via rladmin or CURL)
+# set some keys
+# in a loop, increment keys and save timestamps
+# expose a REST API for current status of looping
+
+echo "Done"
+exit 0
