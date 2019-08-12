b8b114c7b348545cd46feb3c65c7a385ca48a029
Joshua McKenty <jmckenty@gmail.com>
2019-08-12 14:15:55 -0700

    BASH is annoying

--- ci/test.sh
+++ ci/test.sh
@@ -5,8 +5,8 @@
 # set some keys
 # in a loop, increment keys and save timestamps
 # expose a REST API for current status of looping
-export TIMESTAMP=`redis-cli -h $REDIS_HOST --raw time | head -n 1`
-redis-cli -h $REDIS_HOST lpush scimmia $TIMESTAMP
+# export TIMESTAMP=`redis-cli -h $REDIS_HOST --raw time | head -n 1`
+redis-cli -h $REDIS_HOST lpush scimmia `redis-cli -h $REDIS_HOST --raw TIME | head -n 1`
 # LRANGE scimmia -2 2
 # redis-cli -h $REDIS_HOST get foo
 # echo "Done"
