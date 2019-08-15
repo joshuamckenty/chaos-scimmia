fd8d9234be3f0d09a6d0f176d5a4bb952df162fa
Joshua McKenty <jmckenty@gmail.com>
2019-08-14 18:45:31 -0700

    Actual test of time between timestamps

--- ci/scimmia/tests/redis_test.py
+++ ci/scimmia/tests/redis_test.py
@@ -1,5 +1,10 @@
 import nose
 import redis
+import os
 
 def test_redis_timestamps():
-    assert(True)
+    host = os.getenv('REDIS_HOST', "localhost")
+    port = os.getenv('REDIS_PORT', 6379)
+    redis_db = redis.StrictRedis(host=host, port=port, db=0)
+    (last, second) = redis_db.lrange('scimmia', -2, 2)
+    assert (last - second) < 180 # Less than 3 minutes between timestamps
