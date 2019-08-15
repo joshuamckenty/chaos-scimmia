757be4bfa9841b8ff437f51c181374a3787f66f7
Joshua McKenty <jmckenty@gmail.com>
2019-08-15 14:52:27 -0700

    Newest values are the first two

--- ci/scimmia/tests/redis_test.py
+++ ci/scimmia/tests/redis_test.py
@@ -6,5 +6,5 @@ def test_redis_timestamps():
     host = os.getenv('REDIS_HOST', "localhost")
     port = os.getenv('REDIS_PORT', 6379)
     redis_db = redis.StrictRedis(host=host, port=port, db=0)
-    (last, second) = redis_db.lrange('scimmia', -2, 2)
+    (last, second) = redis_db.lrange('scimmia', 0, 1)
     assert (last - second) < 180 # Less than 3 minutes between timestamps
